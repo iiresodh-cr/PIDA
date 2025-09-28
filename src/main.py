052b6bfe6b0092106d3ec787fa68115762c2d19e
- IIRESODH - <webmaster@iiresodh.org>
2025-09-28 12:02:06 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -30,7 +30,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["POST", "GET", "DELETE"],
+    # --- LA ÚNICA CORRECCIÓN ESTÁ EN LA SIGUIENTE LÍNEA ---
+    allow_methods=["POST", "GET", "DELETE", "PATCH"],
     allow_headers=["*"],
 )
 
